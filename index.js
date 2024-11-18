d008397df69b786762389c77a446f4bec14438a1
Batoch <baptiste.massardier@gmail.com>
2024-11-18 20:46:33 +0900

    Fix new EDF API

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 // import fetch from 'node-fetch';
 fetch = require('node-fetch-commonjs');
 
-async function gettodaycolor() {
-  var response = await fetch("https://particulier.edf.fr/services/rest/referentiel/searchTempoStore?dateRelevant=" + getday());
-  const body = await response.text();
-  const data = JSON.parse(body);
-  return data
+async function getdaycolor(day = getday()) {
+  try {
+    var response = await fetch("https://api-commerce.edf.fr/commerce/activet/v1/calendrier-jours-effacement?option=TEMPO&dateApplicationBorneInf=" + getday(1, -1) + "&dateApplicationBorneSup=" + getday(1));
+    
+    const body = await response.text();
+    const data = JSON.parse(body);
+
+    // Find the color of the day from data 
+    return data.content.options[0].calendrier.find(element => element.dateApplication === day).statut
+  }
+  catch (error) {
+    console.error('Error:', error);
+  }
 }
 
 
-function getday() {
+function getday(daytoadd = 0, yeartoadd = 0) {
   const today = new Date();
-  const yyyy = today.getFullYear();
+  const yyyy = today.getFullYear() + yeartoadd;
   let mm = today.getMonth() + 1; // Months start at 0!
-  let dd = today.getDate();
+  let dd = today.getDate() + daytoadd;
   if (dd < 10) dd = '0' + dd;
   if (mm < 10) mm = '0' + mm;
   
@@ -21,4 +29,4 @@ function getday() {
   return formattedToday;
 }
 
-module.exports = {gettodaycolor}
+module.exports = {getdaycolor}
